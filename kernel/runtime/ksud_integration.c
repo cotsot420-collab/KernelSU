771ceeafab57475a088a0d918c1ecd1e9c4c453d
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2026-05-21 18:12:35 +0800

    fix

--- kernel/runtime/ksud_integration.c
+++ kernel/runtime/ksud_integration.c
@@ -369,7 +369,6 @@ append_ksu_rc:
         ret += append_count;
         if (ksu_rc_pos == ksu_rc_len) {
             pr_info("read_iter_proxy: static append done\n");
-            free_module_rc();
         }
     }
 
@@ -383,8 +382,10 @@ append_module_rc:
         pr_info("read_iter_proxy: append module %zu\n", append_count);
         module_rc_pos += append_count;
         ret += append_count;
-        if (module_rc_pos == (ssize_t)module_rc_len)
+        if (module_rc_pos == (ssize_t)module_rc_len) {
             pr_info("read_iter_proxy: module append done\n");
+            free_module_rc();
+        }
     }
     return ret;
 }
